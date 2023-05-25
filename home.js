1a3b10063e412626d1755333814708655cd5c418
Izzatilla <108586467+01Izzat@users.noreply.github.com>
2023-05-25 16:03:20 +0500

    fix err

--- home.js
+++ home.js
@@ -1,22 +1,22 @@
-  window.addEventListener('DOMContentLoaded', (e) => {
-    let email = localStorage.getItem('email')
-    if(!email) {
-      window.location.href = 'http://127.0.0.1:5500/index.html'
-    }
-  })
+window.addEventListener("DOMContentLoaded", (e) => {
+  let email = localStorage.getItem("email");
+  if (!email) {
+    window.location.href = "http://127.0.0.1:5500/index.html";
+  }
+});
 
-  let logoutBtn = document.querySelector('.logout')
-  let elProducts = document.querySelector('.products')
-  let elSearchproducts = document.querySelector('.search__products')
+let logoutBtn = document.querySelector(".logout");
+let elProducts = document.querySelector(".products");
+let elSearchproducts = document.querySelector(".search__products");
 
-  logoutBtn.addEventListener('click', (e) => {
-    localStorage.removeItem('email')
-    window.location.href = "http://127.0.0.1:5500/index.html'"
-  })
+logoutBtn.addEventListener("click", (e) => {
+  localStorage.removeItem("email");
+  window.location.href = "http://127.0.0.1:5500/index.html'";
+});
 
-  function fetchProducts(data) {
-    Array.from(Array(9).keys()).forEach(i => {
-      let loadCard = `
+function fetchProducts(data) {
+  Array.from(Array(9).keys()).forEach((i) => {
+    let loadCard = `
       <div class="card mb-3" aria-hidden="true" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title placeholder-glow">
@@ -31,61 +31,57 @@
           </p>
           <a class="btn btn-primary disabled placeholder col-6"></a>
         </div>
-       </div>`
+       </div>`;
 
-      elProducts.insertAdjacentElement('beforeend', loadCard)
-    })
+    elProducts.insertAdjacentHTML("beforeend", loadCard);
+  });
 
-    fetch('https://api.escuelajs.co/api/v1/products?limit=20&offset=20', {
-      method: 'GET',
-      headers: {'Content-Type': 'application/json'}
-        .then(res => res.json())
-        .then(data => {
-          if(data) {
-            renderFun(data)
-            return;
-          }
-          elProducts.innerHTML = `<h1>Xatolik yuz berdi</h1>`
-        })
-        .catch(error => {
-          elProducts.innerHTML = `<h1>Xatolik yuz berdi</h1>`
-        })
-        .finally(() => {
-
-        }) 
-    })
-  }
-
-  fetchProducts()
-
-
-  function renderFun(data) {
-    elProducts.innerHTML = null
-    data.forEach((item) => {
-      let card = `<div class="card mb-3" style="width: 18rem;">
-      <img src=${item.image[0]} class="card-img-top" alt="...">
-      <div class="card-body d-flex flex-column">
-        <h5 class="card-title">Card title</h5>${item.title}
-        <p class="card-text">${item.description}</p>
-        <p class="card-text text-danger flex-grow-1">Price:$${item.price}
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>`
-
-    elProducts.insertAdjacentElement('beforeend', card)
-    })
-  }
-
-  function fetchNameproduct(evt) {
-    fetch(`https://api.escuelajs.co/api/v1/products/?title=${evt.target.value}`)
-    .then(res => res.json())
-    .then(data => {
-      if(data.length > 0){
-        renderFun()
+  fetch("https://api.escuelajs.co/api/v1/products?limit=20&offset=20", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data) {
+        renderFun(data);
         return;
       }
-      elProducts.innerHTML = `<h1>Bunday ma'lumot topilmadi</h1>`
+      elProducts.innerHTML = `<h1>Xatolik yuz berdi</h1>`;
     })
-  }
-  elSearchproducts.addEventListener('keyup', fetchNameproduct)
-  
+    .catch((error) => {
+      elProducts.innerHTML = `<h1>Xatolik yuz berdi</h1>`;
+    })
+    .finally(() => {});
+}
+
+fetchProducts();
+
+function renderFun(data) {
+  elProducts.innerHTML = null;
+  data.forEach((item) => {
+    let card = `<div class="card mb-3" style="width: 18rem;">
+      <img src=${item?.images[0]} class="card-img-top" alt="...">
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">${item.title}</h5>
+        <p class="card-text flex-grow-1">${item.description}</p>
+        <p class="card-text text-danger">Price:$${item.price}
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>`;
+
+    elProducts.insertAdjacentHTML("beforeend", card);
+  });
+}
+
+function fetchNameproduct(evt) {
+  fetch(`https://api.escuelajs.co/api/v1/products/?title=${evt.target.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.length > 0) {
+        renderFun();
+        return;
+      }
+      elProducts.innerHTML = `<h1>Bunday ma'lumot topilmadi</h1>`;
+    });
+}
+elSearchproducts.addEventListener("keyup", fetchNameproduct);
